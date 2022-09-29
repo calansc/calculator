12bc27a6f8572f48352c5666f7331f0ec6c3baa5
Chad Schroeder <caschroeder@gmail.com>
2022-09-29 14:30:05 -0400

    Add basic math functions and button functionality
    - Link buttons with display
    - Operator buttons save display and operation symbol
    - Equals sends variables through operate function, displays the result
    - Linked assignment in readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # calculator
 The Odin Project: Foundations - Calculator Project
+https://www.theodinproject.com/lessons/foundations-calculator
--- javascript.js
+++ javascript.js
@@ -1,1 +1,69 @@
+function add (a, b) {
 
+    return a + b;
+};
+
+function subtract (a, b) {
+    return a - b;
+};
+
+function multiply (a, b) {
+    return a * b;
+};
+
+function divide (a, b) {
+    if (b === 0) {
+        return 'Divide by O';
+    } else {
+    return a / b;}
+};
+
+function operate (a, b, c) {
+    if (c === '+') {
+       return add(a, b);
+    } else if (c === '-') {
+        return subtract(a, b);
+    } else if (c === '*') {
+        return multiply(a, b);
+    } else if (c === '/') {
+        return divide(a, b);
+    }
+}
+
+let num1 = '';
+let num2 = '';
+let operation = '';
+
+const numButtons = document.querySelectorAll('.number');
+const display = document.getElementById('display');
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent = num1 + button.textContent;
+        num1 = parseInt(display.textContent);
+    })
+})
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    display.textContent = '';
+    num1 = '';
+    num2 = '';
+    //Will also need to clear stored signs / variables here.
+})
+
+const operators = document.querySelectorAll('.operators');
+operators.forEach((button) => {
+    button.addEventListener('click', () => {
+        num2 = num1;
+        operation = button.textContent;
+        console.log(num1, operation);
+        display.textContent = button.textContent;
+        num1 = '';
+    })
+})
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+    display.textContent = operate(num2, num1, operation);
+    num1 = parseInt(display.textContent);
+})
